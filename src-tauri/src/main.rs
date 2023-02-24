c688bb3ee5eb7d1987d5f968f4012a8a611f3d45
Elijah Lopez <elijahllopezz@gmail.com>
2023-02-23 21:47:59 -0500

    redirect in Tauri

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -5,10 +5,16 @@
 )]
 
 use tauri_plugin_window_state;
+use tauri::Manager;
 
 fn main() {
     tauri::Builder::default()
-        .plugin(tauri_plugin_window_state::Builder::default().build())
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+    .plugin(tauri_plugin_window_state::Builder::default().build())
+    .setup(|app| {
+        let window = app.get_window("main").unwrap();
+        window.eval("window.location.replace('https://keep.google.com/');").unwrap();
+        Ok(())
+    })
+    .run(tauri::generate_context!())
+    .expect("error while running tauri application");
 }
